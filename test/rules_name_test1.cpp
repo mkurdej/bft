6f0145e5a600634f1197b2b392378edb814c6cf9
Marek Kurdej <mkurdej@users.noreply.github.com>
2014-09-10 15:59:20 +0200

    Test name for rule_jeffrey_dempster.

--- test/rules_name_test1.cpp
+++ test/rules_name_test1.cpp
@@ -3,8 +3,6 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#define BOOST_TEST_MODULE test_rules
-
 #ifdef _MSC_VER
 #pragma warning(disable : 4505) // unused function
 
@@ -28,28 +26,12 @@
 
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
-#include <boost/bft/mass.hpp>
-#include <boost/math/special_functions/fpclassify.hpp>
 
 #ifdef _MSC_VER
 #pragma warning(pop) // restore original warning level
 #elif defined(__GNUC__)
 #pragma GCC diagnostic pop
 #endif // _MSC_VER
-
-using namespace boost;
-using namespace boost::bft;
-
-BOOST_BFT_DEFINE_CLASS(C1);
-BOOST_BFT_DEFINE_CLASS(C2);
-BOOST_BFT_DEFINE_CLASS(C3);
-BOOST_BFT_DEFINE_CLASS(C4);
-
-typedef fod<C1, C2> fod2;
-typedef fod<C1, C2, C3> fod3;
-
-typedef fod2 fodFO;
-typedef fod3 fodABC;
 
 //==============================================================================
 //#include <boost/bft/conjunctive_decomposition.hpp>
@@ -60,10 +42,13 @@ typedef fod3 fodABC;
 #include <boost/bft/rule_disjunctive.hpp>
 #include <boost/bft/rule_disjunctive_bold.hpp>
 #include <boost/bft/rule_dubois_prade.hpp>
+#include <boost/bft/rule_jeffrey_dempster.hpp>
 #include <boost/bft/rule_normalization.hpp>
 #include <boost/bft/rule_normalization_inagaki.hpp>
 #include <boost/bft/rule_pcr5.hpp>
 #include <boost/bft/rule_yager.hpp>
+
+using namespace boost::bft;
 
 //==============================================================================
 BOOST_AUTO_TEST_SUITE(suite_name)
@@ -84,6 +69,7 @@ BOOST_AUTO_TEST_CASE(test_name)
     BOOST_BFT_CHECK_NAME(rule_disjunctive, "disjunctive rule");
     BOOST_BFT_CHECK_NAME(rule_disjunctive_bold, "bold disjunctive rule");
     BOOST_BFT_CHECK_NAME(rule_dubois_prade, "Dubois-Prade adaptative rule");
+    BOOST_BFT_CHECK_NAME(rule_jeffrey_dempster, "Jeffrey-Dempster revision rule");
     BOOST_BFT_CHECK_NAME(rule_normalization, "normalization rule");
     BOOST_BFT_CHECK_OBJ_NAME(rule_normalization_inagaki(0), "Inagaki normalization rule");
     BOOST_BFT_CHECK_NAME(rule_pcr5, "Proportional Conflict Redistribution Rule no. 5");
