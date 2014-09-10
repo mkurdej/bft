b34c5e38c02f0210749b2ae4629ecd176b308519
Marek Kurdej <mkurdej@users.noreply.github.com>
2014-09-10 17:52:36 +0200

    Fix io.hpp macros.

--- include/boost/bft/io.hpp
+++ include/boost/bft/io.hpp
@@ -21,35 +21,41 @@ namespace detail
 template <typename CharT>
 struct bft_io_traits;
 
-#define BOOST_BFT_DEFINE_TRAITS_MEMBER(CharT, string_prefix, key, value)       \
-    static std::basic_string<CharT> key()                                      \
-    {                                                                          \
-        return BOOST_PP_CAT(string_prefix, value);                             \
-    } \
-                                                                       \
+#define BOOST_BFT_DEFINE_TRAITS_MEMBER(CharT, key, value)   \
+    static std::basic_string<CharT> key()                   \
+    {                                                       \
+        return value;                                       \
+    }                                                       \
 /**/
 
-#define BOOST_BFT_SPECIALIZE_TRAITS(CharT, string_prefix, prefix_, infix_,     \
-                                    suffix_)                                   \
-    template <>                                                                \
-    struct bft_io_traits<CharT>                                                \
-    {                                                                          \
-        BOOST_BFT_DEFINE_TRAITS_MEMBER(CharT, string_prefix, prefix, prefix_)  \
-        BOOST_BFT_DEFINE_TRAITS_MEMBER(CharT, string_prefix, infix, infix_)    \
-        BOOST_BFT_DEFINE_TRAITS_MEMBER(CharT, string_prefix, suffix, suffix_)  \
-    }; \
-                                                                      \
+#define BOOST_BFT_SPECIALIZE_TRAITS(CharT, prefix_, infix_, suffix_)    \
+    template <>                                                         \
+    struct bft_io_traits<CharT>                                         \
+    {                                                                   \
+        BOOST_BFT_DEFINE_TRAITS_MEMBER(CharT, prefix, prefix_)          \
+        BOOST_BFT_DEFINE_TRAITS_MEMBER(CharT, infix, infix_)            \
+        BOOST_BFT_DEFINE_TRAITS_MEMBER(CharT, suffix, suffix_)          \
+    };                                                                  \
 /**/
 
-BOOST_BFT_SPECIALIZE_TRAITS(char, "", "[", ", ", "]")
+#define BOOST_BFT_SPECIALIZE_TRAITS_WITH_PREFIX(CharT, string_prefix,       \
+                                                prefix_, infix_, suffix_)   \
+BOOST_BFT_SPECIALIZE_TRAITS(CharT,          \
+    BOOST_PP_CAT(string_prefix, prefix_),   \
+    BOOST_PP_CAT(string_prefix, infix_),    \
+    BOOST_PP_CAT(string_prefix, suffix_))   \
+/**/
+
+BOOST_BFT_SPECIALIZE_TRAITS(char, "[", ", ", "]")
 #ifndef BOOST_NO_CWCHAR
-BOOST_BFT_SPECIALIZE_TRAITS(wchar_t, L, "[", ", ", "]")
+BOOST_BFT_SPECIALIZE_TRAITS_WITH_PREFIX(wchar_t, L, "[", ", ", "]")
 #endif // BOOST_NO_CWCHAR
 #if !(defined(BOOST_NO_CXX11_CHAR16_T) || defined(BOOST_NO_CXX11_CHAR32_T) || defined(BOOST_NO_CXX11_UNICODE_LITERALS))
-BOOST_BFT_SPECIALIZE_TRAITS(char16_t, u, "[", ", ", "]")
-BOOST_BFT_SPECIALIZE_TRAITS(char32_t, U, "[", ", ", "]")
+BOOST_BFT_SPECIALIZE_TRAITS_WITH_PREFIX(char16_t, u, "[", ", ", "]")
+BOOST_BFT_SPECIALIZE_TRAITS_WITH_PREFIX(char32_t, U, "[", ", ", "]")
 #endif // BOOST_NO_CXX11_UNICODE_LITERALS
 
+#undef BOOST_BFT_SPECIALIZE_TRAITS_WITH_PREFIX
 #undef BOOST_BFT_SPECIALIZE_TRAITS
 #undef BOOST_BFT_DEFINE_TRAITS_MEMBER
 
